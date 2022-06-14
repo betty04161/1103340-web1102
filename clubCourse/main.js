8c4469cf1b1e11703a4c137a1ff02ada0d1e15f3
Betty Sung <yubei710290@gmail.com>
2022-06-14 18:51:38 +0800

    modify

--- clubCourse/main.js
+++ clubCourse/main.js
@@ -1,19 +1,60 @@
 $(function s() {
-    $("#courseTable").append("<tr><th>場次</th><th>時間</th><th>主題</th></tr>");
+  $("#courseTable").append("<tr><th>場次</th><th>時間</th><th>主題</th></tr>");
 
-    var topicCount = topic.length;
+  let days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  let p = document.getElementById("cm").value;
+  let q = document.getElementById("cd").value;
+  setMonthAndDay(p, q);
 
-    // 一秒鐘有1000毫秒
-    // 每分鐘60秒、每小時60分鐘、每天24小時
-    let millisecsPerDay = 24*60*60*1000;
-    for(var x=0; x<topicCount; x++) {
-        debugger;
-        $("#courseTable").append(
-            `<tr>
-            <td>${x+1}</td>
-            <td>${((new Date(startDate.getTime()+7*x*millisecsPerDay)).toLocaleDateString()).slice(5)}</td>
-            <td>${topic[x]}</td>
-            </tr>`
-            );
+  var topicCount = topic.length;
+  // 一秒鐘有1000毫秒
+  // 每分鐘60秒、每小時60分鐘、每天24小時
+  let millisecsPerDay = 24 * 60 * 60 * 1000;
+  for (var x = 0; x < topicCount; x++) {
+    console.log(x);
+    if (x == 0 || x == 1) {
+      $("#courseTable").append(
+        `<tr class="st">
+                <td>${x + 1}</td>
+                <td id="${x}">${new Date(
+          startDate.getTime() + 7 * x * millisecsPerDay
+        )
+          .toLocaleDateString()
+          .slice(5)}</td>
+                <td>${topic[x]}</td>
+                </tr>`
+      );
+    } else {
+      $("#courseTable").append(
+        `<tr>
+                <td>${x + 1}</td>
+                <td id="${x}">${new Date(
+          startDate.getTime() + 7 * x * millisecsPerDay
+        )
+          .toLocaleDateString()
+          .slice(5)}</td>
+                <td>${topic[x]}</td>
+                </tr>`
+      );
     }
+  }
+
+  document.getElementById("button").onclick = function () {
+    let p = document.getElementById("cm").value;
+    let q = document.getElementById("cd").value;
+    if (p > 12 || p < 1) {
+      p = 1;
+    }
+    if (q > days[p - 1] || q < 1) {
+      q = days[p - 1];
+    }
+    setMonthAndDay(p, q);
+    for (var x = 0; x < topicCount; x++) {
+      document.getElementById(x).innerHTML = new Date(
+        startDate.getTime() + 7 * x * millisecsPerDay
+      )
+        .toLocaleDateString()
+        .slice(5);
+    }
+  };
 });
--- clubCourse/topic.js
+++ clubCourse/topic.js
@@ -1,16 +1,15 @@
 var topic = [
-    "尚未開學",
-    "國定假日",
-    "環境準備",
-    "隨機性",
-    "重複性"
+  "尚未開學",
+  "國定假日",
+  "環境準備",
+  "隨機性",
+  "重複性",
+  "條件判斷",
 ];
 var startDate = new Date();
-// debugger;
-function setMonthAndDay(startMonth, startDay){
-    startDate.setMonth(startMonth-1,startDay);
-    startDate.setHours(0);
-    startDate.setMinutes(0);
-    startDate.setSeconds(0);
+function setMonthAndDay(startMonth, startDay) {
+  startDate.setMonth(startMonth - 1, startDay);
+  startDate.setHours(0);
+  startDate.setMinutes(0);
+  startDate.setSeconds(0);
 }
-setMonthAndDay(2,21);
